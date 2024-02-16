3bf2e8e6e9df819d35fe7f04f052b6ad8fb67b0f
testapplicationone <160116575+testapplicationone@users.noreply.github.com>
2024-02-16 14:15:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.figure_factory as ff
 from PIL import Image
 
 options = st.multiselect(
@@ -17,4 +18,19 @@ chart_data = pd.DataFrame(
 
 st.line_chart(chart_data)
 
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
